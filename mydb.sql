24bca66b3a5ba26b424b88e61a1d49e4437fe6a1
DESKTOP-5J9365U\gy <lgy6160@gmail.com>
2023-06-16 16:02:01 +0900

    강의 공지사항

--- mydb.sql
+++ mydb.sql
@@ -11,18 +11,17 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8mb3 ;
 USE `mydb` ;
 
 -- -----------------------------------------------------
 -- Table `mydb`.`professor_tbl`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`professor_tbl` ;
-
 CREATE TABLE IF NOT EXISTS `mydb`.`professor_tbl` (
-  `p_no` INT NOT NULL,
-  `p_name` VARCHAR(45) NULL DEFAULT NULL,
-  `p_psw` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`p_no`))
+  `pro_no` INT NOT NULL COMMENT '교수번호',
+  `pro_name` VARCHAR(45) NOT NULL COMMENT '교수이름',
+  `pro_pwd` VARCHAR(45) NOT NULL COMMENT '교수비번',
+  PRIMARY KEY (`pro_no`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
@@ -30,70 +29,28 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- -----------------------------------------------------
 -- Table `mydb`.`student_tbl`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`student_tbl` ;
-
 CREATE TABLE IF NOT EXISTS `mydb`.`student_tbl` (
-  `s_no` INT NOT NULL,
-  `s_name` VARCHAR(45) NULL DEFAULT NULL,
-  `s_pwd` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`s_no`))
+  `stu_no` INT NOT NULL COMMENT '학번',
+  `stu_name` VARCHAR(45) NOT NULL COMMENT '학생이름',
+  `stu_pwd` VARCHAR(45) NOT NULL COMMENT '학생비번',
+  PRIMARY KEY (`stu_no`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`attend_tbl`
+-- Table `mydb`.`subject_tbl`
 -- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `mydb`.`attend_tbl` (
-  `s_no` INT NOT NULL,
-  `sub_code` VARCHAR(5) NULL DEFAULT NULL,
-  `p_no` INT NOT NULL,
-  `att_grade` INT NULL DEFAULT '0',
-  PRIMARY KEY (`s_no`, `p_no`),
-  INDEX `fk_attend_p_no_idx` (`p_no` ASC) VISIBLE,
-  CONSTRAINT `fk_attend_p_no`
-    FOREIGN KEY (`p_no`)
-    REFERENCES `mydb`.`professor_tbl` (`p_no`),
-  CONSTRAINT `fk_attend_s_no`
-    FOREIGN KEY (`s_no`)
-    REFERENCES `mydb`.`student_tbl` (`s_no`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`lecture_tbl`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`lecture_tbl` ;
-
-CREATE TABLE IF NOT EXISTS `mydb`.`lecture_tbl` (
-  `l_id` INT NOT NULL,
-  `l_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`l_id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`board_tbl`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`board_tbl` ;
-
-CREATE TABLE IF NOT EXISTS `mydb`.`board_tbl` (
-  `b_id` INT NOT NULL AUTO_INCREMENT,
-  `lecture_id` INT NULL,
-  `crator_id` INT NOT NULL,
-  `b_title` VARCHAR(150) NOT NULL,
-  `content` TEXT NOT NULL,
-  PRIMARY KEY (`b_id`),
-  INDEX `fd_board_crator_id_idx` (`crator_id` ASC) VISIBLE,
-  INDEX `fkl_board_lecture_id_idx` (`lecture_id` ASC) VISIBLE,
-  CONSTRAINT `fk_board_crator_id`
-    FOREIGN KEY (`crator_id`)
-    REFERENCES `mydb`.`professor_tbl` (`p_no`),
-  CONSTRAINT `fkl_board_lecture_id`
-    FOREIGN KEY (`lecture_id`)
-    REFERENCES `mydb`.`lecture_tbl` (`l_id`)
+CREATE TABLE IF NOT EXISTS `mydb`.`subject_tbl` (
+  `sub_code` VARCHAR(5) NOT NULL COMMENT '과목코드',
+  `sub_name` VARCHAR(45) NOT NULL COMMENT '과목명',
+  `pro_no` INT NULL COMMENT '교수번호',
+  PRIMARY KEY (`sub_code`),
+  UNIQUE INDEX `sub_code_UNIQUE` (`sub_code` ASC) VISIBLE,
+  INDEX `fk_subject_tbl_pro_no_idx` (`pro_no` ASC) VISIBLE,
+  CONSTRAINT `fk_subject_tbl_pro_no`
+    FOREIGN KEY (`pro_no`)
+    REFERENCES `mydb`.`professor_tbl` (`pro_no`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -101,16 +58,65 @@ DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
+-- Table `mydb`.`attend_tbl`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`attend_tbl` (
+  `stu_no` INT NOT NULL COMMENT '학번',
+  `sub_code` VARCHAR(5) NULL DEFAULT NULL COMMENT '과목코드',
+  `pro_no` INT NOT NULL COMMENT '교수번호',
+  `att_year` INT NULL DEFAULT NULL COMMENT '수강년도',
+  `att_term` INT NULL DEFAULT NULL COMMENT '수강학기',
+  `att_grade` VARCHAR(45) NULL DEFAULT '0' COMMENT '취득점수',
+  PRIMARY KEY (`stu_no`, `pro_no`),
+  INDEX `fk_attend_p_no_idx` (`pro_no` ASC) VISIBLE,
+  INDEX `fk_attend_sub_code_idx` (`sub_code` ASC) VISIBLE,
+  CONSTRAINT `fk_attend_pro_no`
+    FOREIGN KEY (`pro_no`)
+    REFERENCES `mydb`.`professor_tbl` (`pro_no`),
+  CONSTRAINT `fk_attend_stu_no`
+    FOREIGN KEY (`stu_no`)
+    REFERENCES `mydb`.`student_tbl` (`stu_no`),
+  CONSTRAINT `fk_attend_sub_code`
+    FOREIGN KEY (`sub_code`)
+    REFERENCES `mydb`.`subject_tbl` (`sub_code`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`board_tbl`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`board_tbl` (
+  `bod_no` INT NOT NULL AUTO_INCREMENT COMMENT '게시글 번호',
+  `sub_code` VARCHAR(5) NOT NULL COMMENT '교과목 번호',
+  `pro_no` INT NOT NULL COMMENT '교수번호(작성자)',
+  `bod_title` VARCHAR(150) NOT NULL COMMENT '게시글 제목',
+  `bod_content` TEXT NOT NULL COMMENT '게시글 본문',
+  `bod_image` VARCHAR(200) NULL DEFAULT NULL COMMENT '게시글 이미지',
+  `bod_regdate` DATETIME NULL DEFAULT NULL COMMENT '게시글 등록일자',
+  `bod_update` DATETIME NULL DEFAULT NULL COMMENT '게시글 수정일자',
+  `bod_deldate` DATETIME NULL DEFAULT NULL COMMENT '게시글 삭제일자',
+  PRIMARY KEY (`bod_no`),
+  INDEX `fd_board_crator_id_idx` (`pro_no` ASC) VISIBLE,
+  CONSTRAINT `fk_bod_pro_no`
+    FOREIGN KEY (`pro_no`)
+    REFERENCES `mydb`.`professor_tbl` (`pro_no`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
 -- Table `mydb`.`score_tbl`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`score_tbl` ;
-
 CREATE TABLE IF NOT EXISTS `mydb`.`score_tbl` (
-  `s_no` INT NOT NULL,
-  PRIMARY KEY (`s_no`),
-  CONSTRAINT `fk_score_s_no`
-    FOREIGN KEY (`s_no`)
-    REFERENCES `mydb`.`student_tbl` (`s_no`))
+  `stu_no` INT NOT NULL COMMENT '학번',
+  `sco_year` INT NOT NULL COMMENT '성적취득년도',
+  `sco_term` INT NOT NULL COMMENT '학기',
+  `sco_avg` INT NULL DEFAULT NULL COMMENT '평점평균',
+  PRIMARY KEY (`stu_no`, `sco_year`, `sco_term`),
+  CONSTRAINT `fk_score_stu_no`
+    FOREIGN KEY (`stu_no`)
+    REFERENCES `mydb`.`student_tbl` (`stu_no`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
